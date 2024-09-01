6af2da4f299f58473b7cf4cacc21bbfc1ffe920c
nichoth <nichoth@nichoth.com>
2024-09-01 10:06:41 -0700

    better types

--- src/index.d.ts
+++ src/index.d.ts
@@ -14,20 +14,21 @@ type TonicComponent = (()=>TonicTemplate|HTMLElement)|typeof Tonic
 * Class Tonic
 */
 export class Tonic<T = {}> extends HTMLElement {
-   static _tags: string;
-   static _refIds: any[];
-   static _data: {};
-   static _states: {};
-   static _children: {};
-   static _reg: {};
-   static _stylesheetRegistry: any[];
-   static _index: number;
-   static get version(): any;
-   static get SPREAD(): RegExp;
-   static get ESC(): RegExp;
-   static get AsyncFunctionGenerator(): Function;
-   static get AsyncFunction(): Function;
-   static get MAP(): {
+   static _tags:string;
+   static _refIds:any[];
+   static _data:{};
+   static _states:{};
+   static _children:{};
+   static _reg:{};
+   static _stylesheetRegistry:any[];
+   static _index:number;
+   static event (type:string):string;
+   static get version():string;
+   static get SPREAD():RegExp;
+   static get ESC():RegExp;
+   static get AsyncFunctionGenerator():Function;
+   static get AsyncFunction():Function;
+   static get MAP():{
        '"': string;
        '&': string;
        '\'': string;
@@ -36,11 +37,11 @@ export class Tonic<T = {}> extends HTMLElement {
        '`': string;
        '/': string;
    };
-   static ssr: any;
-   static nonce: any;
-   static _createId(): string;
+   static ssr:boolean;
+   static nonce:string;
+   static _createId():string;
    static _normalizeAttrs(o: any, x?: {}): {};
-   static match(el:any, s:any):any;
+   static match(el:HTMLElement, s:string):HTMLElement|null;
    static getTagName(camelName:string):string;
    static get tag():`${string}-${string}`;
    static getPropertyNames(proto:any):string[];
@@ -50,45 +51,45 @@ export class Tonic<T = {}> extends HTMLElement {
     * @param {string} [htmlName] Name of the element, default to the class name
     * @returns {void}
     */
-   static add(c: TonicComponent, htmlName?: string): void;
-   static registerStyles(stylesheetFn: any): void;
-   static escape(s: any): any;
-   static unsafeRawString(s: any, templateStrings: any): TonicTemplate;
+   static add(c:TonicComponent, htmlName?:string):void;
+   static registerStyles(stylesheetFn:()=>string):void;
+   static escape(s:string):string;
+   static unsafeRawString(s:string, templateStrings:string[]):TonicTemplate;
    /**
     * @type {Element[] & { __children__? }}
     * @private
     */
-   private elements;
+   private elements:Element[] & { __children__? };
    /**
     * @type {ChildNode[] & { __children__? }}
     * @private
     */
-   private nodes;
-   _props: any; /** @private */
-   _state: any;
-   preventRenderOnReconnect: boolean;
+   private nodes:ChildNode[] & { __children__? };
+   _props:any; /** @private */
+   _state:any;
+   preventRenderOnReconnect:boolean;
    props:T & Partial<{ class; id; }>;
    /**
     * Render the component.
     * @abstract
     * @return {TonicTemplate|Promise<TonicTemplate>}
     */
-   render(): TonicTemplate | Promise<TonicTemplate>;
-   get isTonicComponent(): boolean;
-   _checkId(): string;
+   render():TonicTemplate|Promise<TonicTemplate>;
+   get isTonicComponent():boolean;
+   _checkId():string;
    set state(newState: any);
-   get state(): any;
-   _events(): void;
-   _prop(o: any): string;
-   _placehold(r: any): string;
+   get state():any;
+   _events():void;
+   _prop(o:any):string;
+   _placehold(r:any):string;
    dispatch(eventName:string, detail?:any):void;
    emit(eventName:string, detail?:any):void;
-   html(strings: any, ...values: any[]): TonicTemplate;
-   scheduleReRender(oldProps: any): any;
-   pendingReRender: any;
-   reRender(o?: {}): any;
-   handleEvent(e: any): void;
-   _drainIterator(target: any, iterator: any): any;
+   html(strings:any, ...values:any[]):TonicTemplate;
+   scheduleReRender(oldProps:any):any;
+   pendingReRender:any;
+   reRender(o?:{}):any;
+   handleEvent(e: any):void;
+   _drainIterator(target:any, iterator:any):any;
    /**
     * _set
     * @param {Element|InstanceType<typeof Tonic>|ShadowRoot} target
@@ -98,22 +99,22 @@ export class Tonic<T = {}> extends HTMLElement {
     * @private
     */
    private _set;
-   _apply(target: any, content: any): void;
-   connectedCallback(): Promise<any>;
-   root: any;
-   _id: any;
-   _source: string;
-   isInDocument(target: any): boolean;
-   disconnectedCallback(): void;
+   _apply(target:any, content:any):void;
+   connectedCallback():Promise<any>;
+   root:any;
+   _id:any;
+   _source:string;
+   isInDocument(target:any):boolean;
+   disconnectedCallback():void;
 }
 export default Tonic;
 
 export class TonicTemplate {
-   constructor(rawText: any, templateStrings: any, unsafe: any);
+   constructor(rawText:any, templateStrings:any, unsafe:any);
    isTonicTemplate: boolean;
-   unsafe: any;
-   rawText: any;
-   templateStrings: any;
-   valueOf(): any;
-   toString(): any;
+   unsafe:any;
+   rawText:any;
+   templateStrings:any;
+   valueOf():any;
+   toString():any;
 }
